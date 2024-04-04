3fc6f39b4a3925b56754b479c4965663c3322a4e
abhixyz1 <abhinayazrf@gmail.com>
2024-04-04 08:03:05 +0700

    Praktikum Searching

---
+++ MergeSortTEst/SortMain.java
@@ -0,0 +1,17 @@
+package MergeSortTest;
+/**
+ * @author Abhinaya
+ */
+public class SortMain {
+    
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("Sorting dengan Merge Sort");
+        MergeSorting mSort = new MergeSorting();
+        System.out.println("Data Awal");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("Setelah diurutkan");
+        mSort.printArray(data);
+    }
+}
